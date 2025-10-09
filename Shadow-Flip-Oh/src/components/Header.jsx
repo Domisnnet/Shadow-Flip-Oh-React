6e7a38f97d437e717addb7a6004a0fb204580725
Domisnnet <dominique.goncalves61@gmail.com>
2025-10-09 16:26:45 -0300

    ajustes

--- Shadow-Flip-Oh/src/components/Header.jsx
+++ Shadow-Flip-Oh/src/components/Header.jsx
@@ -2,11 +2,11 @@
 import React from 'react';
 
 function Header() {
-  return (
-    <header className="cabecalho">
-      <h1 className="titulo">Shadow-Flip-Oh!</h1>
-    </header>
-  );
+    return (
+        <header className="cabecalho">
+            <h1 className="titulo">Shadow-Flip-Oh!</h1>
+        </header>
+    );
 }
 
-export default Header;
+export default Header; 
