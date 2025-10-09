9c12ba9c1e6816c16467562dbaaa3a493553dc3f
Domisnnet <dominique.goncalves61@gmail.com>
2025-10-09 14:42:34 -0300

    alteração em app.jsx

--- Shadow-Flip-Oh/src/App.jsx
+++ Shadow-Flip-Oh/src/App.jsx
@@ -1,34 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css'; // Mantenha a importação do CSS (iremos mudar o conteúdo dele)
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      {/* Aqui é onde seu código vai entrar */}
+      <h1>Yu-Gi-Oh! Slider</h1>
+    </div>
   )
 }
 
